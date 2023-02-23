55ba0bc9e4e4fa119f09ed283ea55d4dd13027f8
Rabiah263 <aliyurabi262@gmail.com>
2023-02-23 18:10:31 +0000

    Infinite loop avoided

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - test that the function of an integer whether positive_or_negative.
- * @i the integer to be checked
+ * main - test the function of an integer whether positive_or_negative.
  * Return: 0
  */
 int main(void)
@@ -11,5 +10,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
